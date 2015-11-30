18d0b26a9c6989650f42fbe4664d72dd49b95bea
Schkred <schkredt@gmail.com>
2015-11-30 15:02:36 +0200

    Finished Guess the number game

--- guessNumber.py
+++ guessNumber.py
@@ -3,16 +3,13 @@ import random
 guessed_number = random.randrange(1, 100, 1)
 def decider (*x) :
 	while x != guessed_number :
-		x = raw_input("Enter guessed number (from 1 to 100): ")
+		x = int(raw_input("Enter guessed number (from 1 to 100): "))
 		if x < guessed_number :
 			print("Wrong! Guessed number is greater. Try again")
-			continue
 		elif x > guessed_number :
 			print("Not this time fella. Guessed number is smaller. Try again")
-			continue
 		elif x == guessed_number :
 			print("Bingo! You guessed the number. Now go and take a cookie")
-			break
 decider()
 
 
