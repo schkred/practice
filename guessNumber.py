0716bbd75e3c05df462e15fffc2e28396f391b9b
Schkred <schkredt@gmail.com>
2015-11-30 13:00:00 +0200

    Edited guessNumber.py

--- guessNumber.py
+++ guessNumber.py
@@ -1,12 +1,20 @@
 import random
 
-x = raw_input("Enter guessed number: ")
 guessed_number = random.randrange(1, 100, 1)
-if x < guessed_number :
-	print("Wrong! Guessed number is greater. Try again")
-elif x > guessed_number :
-	print("Not this time! Guessed number is smaller. Try again")
-elif x == guessed_number :
-	print("Bingo! You guessed the number. Now go and take a cookie")
+def decider (*x) :
+	while x != guessed_number :
+		x = raw_input("Enter guessed number (from 1 to 100): ")
+		print guessed_number
+		if x < guessed_number :
+			print("Wrong! Guessed number is greater. Try again")
+			continue
+		elif x > guessed_number :
+			print("Not this time fella. Guessed number is smaller. Try again")
+			continue
+		elif x == guessed_number :
+			print("Bingo! You guessed the number. Now go and take a cookie")
+			break
+decider()
+
 
 
