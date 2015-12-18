c410d180645382d37cb75f952d525993c9480e00
schkred <schkredt@gmail.com>
2015-12-18 23:38:47 +0200

    delete temporary work files

--- dict2.py
+++
@@ -1,8 +0,0 @@
-class AttrDict(dict) :
-	def __init__(self, key, value) :
-		self.key = key
-		self.value = value
-	def __getitem__(self, key) :
-		return self.value
-	def __setitem__(self, key, value) :
-		return 
